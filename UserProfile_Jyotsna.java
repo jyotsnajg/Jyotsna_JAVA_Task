f8e738aedb32ca21537a6678a081bb4f808423da
jyotsnajg <jyotsnalc@gmail.com>
2025-07-11 01:38:31 +0530

    Create  UserProfile_Jyotsna.java

---
+++ UserProfile_Jyotsna.java
@@ -0,0 +1,16 @@
+public class UserProfile_Jyotsna {
+
+public static void main (String[] args) {
+// variable declaration 
+int camapign_id = 12345;
+string campaign_name = 'BOLD_MG_Camapign';
+double amount = 9362929.39;
+char gender = F;
+boolean isAvailable = true;
+System.out.println("User Profile Jyotsna");
+System.out.println("Campaign_id" : +camapign_id);
+System.out.println("Campaign_name" : +camapign_name);
+System.out.println("Amount" : +amount);
+System.out.println("Gender: " + gender);
+System.out.println(isAvailable);
+} }
